fb57014493d60da908d1915969aba8c42d1522b1
Dohiii <77291884+Dohiii@users.noreply.github.com>
2021-05-22 17:50:43 +0200

    id

--- github.js
+++ github.js
@@ -2,14 +2,19 @@ class GitHub {
     constructor() {
         this.client_id = 'fe9140f180cc2628278e';
         this.client_secret = 'fff6248842fb81344dc47741201ea5c92465816e';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
--- ui.js
+++ ui.js
@@ -30,6 +30,31 @@ class UI {
 <div id="repos"></div>
         `;
     }
+
+    //Show user repos
+    showRepos(repos) {
+        let output = '';
+        repos.forEach(repo => {
+            output += `
+            <div class="card card-body mb-2">
+            <div class="row">
+            <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge bg-primary">Stars: ${repo.starfazers_count}</span>
+            <span class="badge bg-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge bg-success">Forks: ${repo.forks_count}</span>
+            </div>
+            </div>
+            </div>
+            `;
+        });
+
+        //output repos
+        document.getElementById('repos').innerHTML = output;
+    }
+
     //Show alert message
     showAlert(message, className) {
         //clear any remaining alrets
